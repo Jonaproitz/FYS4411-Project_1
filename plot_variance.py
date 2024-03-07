7ac9b5be575c539633e00eac365e6355c8b73665
jonaproitz <jonaproitz@gmail.com>
2024-03-07 14:55:08 +0100

    Add figures folder

--- plot_variance.py
+++ plot_variance.py
@@ -5,4 +5,5 @@ filename = "ExpectationValues.txt"
 alpha, energy, variance = np.loadtxt(filename, unpack=True)
 
 plt.plot(alpha, variance)
-plt.show()
+plt.savefig("doc/figures/Varience_B.png")
+plt.close()
