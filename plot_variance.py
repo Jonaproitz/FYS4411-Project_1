46aab21379f5b3b7cfce188ece3d7a6f0cad595e
jonaproitz <jonaproitz@gmail.com>
2024-04-01 15:52:16 +0200

    Add labels and title to plot

--- plot_variance.py
+++ plot_variance.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = "ExpectationValues.txt"
+filename = "Results/ExpectationValues_problem_b.txt"
 alpha, energy, variance = np.loadtxt(filename, unpack=True)
 
+plt.figure()
+plt.title("Variance as a function of parameter $\\alpha$")
 plt.plot(alpha, variance)
+plt.xlabel("$\\alpha$")
+plt.ylabel("Variance")
+plt.grid()
 plt.savefig("doc/figures/Varience_B.png")
 plt.close()
